8ba3b5a0f6669680c08732810fab15822d536386
Alvaro Cañón <acanoc01@estudiantes.unileon.es>
2020-04-17 15:39:54 +0200

    seguimosss

---
+++ APIC-EM/APIC_ticket.py
@@ -0,0 +1,77 @@
+import json
+import requests
+
+
+# Es de tipo singleton
+
+
+class APICEM_ticket:
+    __instance = None
+
+    @staticmethod
+    def getInstance():
+        """ Static access method. """
+        if APICEM_ticket.__instance == None:
+            raise Exception(
+                "This is the first Instance use firstInstance method")
+        return APICEM_ticket.__instance
+
+    @staticmethod
+    def firstInstance(username, password, api_url):
+        """ Static access method. """
+        if APICEM_ticket.__instance == None:
+            APICEM_ticket(username, password, api_url)
+        return APICEM_ticket.__instance
+
+    def __init__(self, username, password, api_url):
+        """ Virtually private constructor. """
+        if APICEM_ticket.__instance != None:
+            raise Exception("This class is a singleton!")
+        else:
+            self.ticket = self.create_ticket(username, password, api_url)
+            self.username = username
+            self.password = password
+            self.api_url = api_url
+            APICEM_ticket.__instance = self
+
+    def create_ticket(self, username, password, api_url):
+        try:
+            print("Pidiendo ticket, por favor espere...")
+            requests.packages.urllib3.disable_warnings()
+            api_url = api_url + "/api/v1/ticket"
+            headers = {
+                "content-type": "application/json"
+            }
+            body_json = {
+                "username": str(username),
+                "password": str(password)
+            }
+
+            resp = requests.post(api_url, json.dumps(body_json),
+                                 headers=headers, verify=False)
+
+            response_json = resp.json()
+            serviceTicket = response_json["response"]["serviceTicket"]
+
+            response_json = resp.json()
+            serviceTicket = response_json["response"]["serviceTicket"]
+
+            return serviceTicket
+
+        except:
+
+            if(resp.status_code == 401):
+                print("Error en el Login")
+                return "-1"
+
+    def get_ticket(self):
+        return self.ticket
+
+    def set_password(self, password):
+        self.password = password
+
+    def set_apiurl(self, api_url):
+        self.api_url = api_url
+
+    def set_username(self, username):
+        self.username = username
---
+++ APIC-EM/main.py
@@ -0,0 +1,107 @@
+from tabulate import *
+import json
+import requests
+from APIC_ticket import *
+
+
+APICEM_DATA = {
+    "username": "devnetuser",
+    "password": "Xj3BDqbU",
+    "api_url": "https://DevnetSBX-NetAcad-APICEM-3.cisco.com"
+}
+
+
+def setCredential():
+    username = input("Introduzca username->")
+    password = input("Introduzca password->")
+
+    APICEM_DATA["username"] = username
+    APICEM_DATA["password"] = password
+
+
+def setCredentialURL():
+    username = input("Introduzca username->")
+    password = input("Introduzca password->")
+    api_url = input(
+        "Introduzca URL (Ejemplo:https://DevnetSBX-NetAcad-APICEM-3.cisco.com)->")
+
+    APICEM_DATA["username"] = username
+    APICEM_DATA["password"] = password
+    APICEM_DATA["api_url"] = api_url
+
+
+def setDefaultCredential():
+    pass
+
+
+def getNetworkHostInventory():
+    pass
+
+
+def main():
+
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    print("~~ Bienvedio al controlador APIC-EM! ~~")
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n")
+
+    while True:
+        print("=====================================")
+        print("========>    MENÚ APIC-EM    <=======")
+        print("=====================================")
+
+        print("1) Añadir credenciales y/o URL")
+        print("2) Usar credenciales y URL por defecto")
+        print("3) Listar los Host de la red")
+        print("X) Pulse cualquier otra tecla para salir")
+
+        eleccion = input("Escoja opción->")
+        switcher = {
+
+            "1": getNetworkHostInventory,
+
+            "2": "xxx",
+        }
+
+        try:
+
+            switcher[eleccion]()  # para llamar a las funciones
+
+            print("\n\n")
+
+        except:
+            print("BYE BYE\n")
+            break
+
+
+if __name__ == "__main__":
+
+    print("ESTABLECER CREDENCIALES PARA ACCEDER APIC-EM")
+    print("Menú LOGIN APIC-EM:")
+    print("1) Logearse en url por defecto")
+    print("2) Logearse en url nueva")
+    print("3) Emplear Login guardada y url por defecto")
+    print("X) Pulse cualquier otra tecla para salir")
+
+    eleccion = input("Escoja opción->")
+
+    switcher = {
+        "1": setCredential,
+
+        "2": setCredentialURL,
+
+        "3": setDefaultCredential,
+    }
+
+    switcher[eleccion]()  # para llamar a las funciones
+    print("\n\n")
+
+    try:
+        apicObj = APICEM_ticket.firstInstance(APICEM_DATA["username"],
+                                              APICEM_DATA["password"],
+                                              APICEM_DATA["api_url"])
+
+        ticket = apicObj.get_ticket()
+        if(str(ticket) != "-1"):
+            main()
+    except:
+        print("Error durante el acceso a la página")
